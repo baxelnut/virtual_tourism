5ba22144a362abccdc77f938a7779291d991b036
Basilius Tengang <basiliustengang24@gmail.com>
2025-01-29 23:55:06 +0700

    review section ui adjustment

--- lib/src/components/review_section.dart
+++ lib/src/components/review_section.dart
@@ -7,121 +7,172 @@ class ReviewSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final screenSize = MediaQuery.of(context).size;
-    final List<int> numbersData = [23, 5, 4, 2, 0];
+    final List<int> ratings = [23, 5, 4, 2, 0];
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.only(bottom: 20, top: 50),
             child: Text(
               'Rating & Reviews',
-              style: theme.textTheme.headlineMedium,
+              style: theme.textTheme.headlineSmall,
             ),
           ),
-          statsWidget(
-            screenSize: screenSize,
-            theme: theme,
-            numbersData: numbersData,
+          _buildRatingStat(
+            screenSize,
+            theme,
+            ratings,
           ),
-          Row(
-            children: [
-              Text(
-                'Write a review',
-                style: theme.textTheme.titleMedium,
-              ),
-              const SizedBox(width: 10),
-              const Icon(
-                Icons.rate_review_rounded,
-                size: 22,
-              )
-            ],
+          _buildReviewWidget(
+            screenSize,
+            theme,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: theme.colorScheme.primary,
+            ),
+            child: Icon(
+              Icons.comment,
+              size: 22,
+              color: theme.colorScheme.onPrimary,
+            ),
           ),
         ],
       ),
     );
   }
 
-  Widget statsWidget({
-    required Size screenSize,
-    required ThemeData theme,
-    required List<int> numbersData,
-  }) {
+  Widget _buildReviewWidget(
+    Size screenSize,
+    ThemeData theme,
+  ) {
+    return Column(
+      children: [
+        Text(
+          'X reviews',
+          style: theme.textTheme.titleSmall,
+        ),
+        ListTile(
+          trailing: const CircleAvatar(
+            radius: 24,
+          ),
+          title: Text(
+            '50 Cent',
+            style: theme.textTheme.titleMedium,
+            textAlign: TextAlign.end,
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'this app is so good! will definitely recommend to my homies and ops❤️',
+                style: theme.textTheme.labelMedium,
+                textAlign: TextAlign.end,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                '30 Jan 2025',
+                style: theme.textTheme.labelMedium,
+              ),
+            ],
+          ),
+        ),
+        ListTile(
+          trailing: const CircleAvatar(
+            radius: 24,
+          ),
+          title: Text(
+            '50 Cent',
+            style: theme.textTheme.titleMedium,
+            textAlign: TextAlign.end,
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'this app is so good! will definitely recommend to my homies and ops❤️',
+                style: theme.textTheme.labelMedium,
+                textAlign: TextAlign.end,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                '30 Jan 2025',
+                style: theme.textTheme.labelMedium,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildRatingStat(
+    Size screenSize,
+    ThemeData theme,
+    List<int> ratings,
+  ) {
     return SizedBox(
       width: screenSize.width,
       height: 130,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          averageScore(
-            theme: theme,
-            numbersData: numbersData,
-          ),
+          _buildAverageScore(theme, ratings),
           Row(
             children: [
-              starsOrder(),
+              _buildStarsOrder(),
               const SizedBox(width: 8),
-              indicatorBar(
-                theme: theme,
-                numbersData: numbersData,
-              ),
+              _buildIndicatorBar(theme: theme, ratings: ratings),
             ],
           ),
-          reviewerQty(
-            theme: theme,
-            numbersData: numbersData,
-          ),
+          _buildReviewerQty(theme, ratings),
         ],
       ),
     );
   }
 
-  Widget averageScore({
-    required ThemeData theme,
-    required List<int> numbersData,
-  }) {
-    int totalStars = numbersData.reduce((a, b) => a + b); // total
-    Map<int, int> starMap = {0: 5, 1: 4, 2: 3, 3: 2, 4: 1}; // mapping stars
-    int weightedSum = 0; // weighted sum
-    for (int i = 0; i < numbersData.length; i++) {
-      weightedSum += starMap[i]! * numbersData[i];
+  Widget _buildAverageScore(ThemeData theme, List<int> ratings) {
+    final totalRatings = ratings.reduce((a, b) => a + b);
+    final starMap = {0: 5, 1: 4, 2: 3, 3: 2, 4: 1};
+    int weightedSum = 0;
+
+    for (int i = 0; i < ratings.length; i++) {
+      weightedSum += starMap[i]! * ratings[i];
     }
-    // average score
-    double starScore = totalStars > 0 ? weightedSum / totalStars : 0;
+
+    final averageScore = totalRatings > 0 ? weightedSum / totalRatings : 0;
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Text(
-          starScore.toStringAsFixed(1),
+          averageScore.toStringAsFixed(1),
           style: theme.textTheme.displayMedium,
         ),
         Text(
-          '$totalStars ratings',
+          '$totalRatings ratings',
           style: theme.textTheme.labelMedium,
         ),
       ],
     );
   }
 
-  Widget starsOrder() {
+  Widget _buildStarsOrder() {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: List.generate(
         5,
         (index) {
-          int starCount = 5 - index;
+          final starCount = 5 - index;
           return Row(
             children: [
-              ...List.generate(index, (i) => const SizedBox(width: 22)),
+              ...List.generate(index, (_) => const SizedBox(width: 22)),
               ...List.generate(
                 starCount,
-                (i) => const Icon(
+                (_) => const Icon(
                   Icons.star_rate,
                   size: 22,
                   color: Colors.amber,
@@ -134,20 +185,31 @@ class ReviewSection extends StatelessWidget {
     );
   }
 
-  Widget indicatorBar({
+  Widget _buildReviewerQty(ThemeData theme, List<int> ratings) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: List.generate(ratings.length, (index) {
+        return Text(
+          ratings[index] > 999 ? '999+' : ratings[index].toString(),
+          style: theme.textTheme.titleMedium,
+        );
+      }),
+    );
+  }
+
+  Widget _buildIndicatorBar({
     required ThemeData theme,
-    required List<int> numbersData,
+    required List<int> ratings,
   }) {
-    int totalRatings = numbersData.reduce((a, b) => a + b);
-    List<double> percentages = numbersData.map((count) {
-      return totalRatings > 0 ? (count / totalRatings) * 100 : 0.0;
-    }).toList();
+    final totalRatings = ratings.reduce((a, b) => a + b);
+    final percentages = ratings
+        .map((count) => totalRatings > 0 ? (count / totalRatings) * 100 : 0.0)
+        .toList();
 
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: List.generate(5, (index) {
-        double percentage = percentages[index];
+        final percentage = percentages[index];
 
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 8),
@@ -161,22 +223,6 @@ class ReviewSection extends StatelessWidget {
               ),
             ),
           ),
-        );
-      }),
-    );
-  }
-
-  Widget reviewerQty({
-    required List<int> numbersData,
-    required ThemeData theme,
-  }) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: List.generate(numbersData.length, (index) {
-        return Text(
-          numbersData[index] > 999 ? '999+' : numbersData[index].toString(),
-          style: theme.textTheme.titleMedium,
         );
       }),
     );
