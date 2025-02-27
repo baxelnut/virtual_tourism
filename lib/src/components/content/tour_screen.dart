5a42f17c33fbd17e8ed143428efb6ee41288a5a6
Basilius Tengang <basiliustengang24@gmail.com>
2025-02-27 10:51:39 +0700

    tour page revision, removing _showDebugInfo

--- lib/src/components/content/tour_screen.dart
+++ lib/src/components/content/tour_screen.dart
@@ -13,85 +13,82 @@ class TourScreen extends StatefulWidget {
 }
 
 class _TourScreenState extends State<TourScreen> {
-  bool _showDebugInfo = false;
-  double _lon = 0;
-  double _lat = 0;
-  double _tilt = 0;
+  // bool _showDebugInfo = false;
+  // double _lon = 0;
+  // double _lat = 0;
+  // double _tilt = 0;
   int _panoId = 0;
 
-  void onViewChanged(longitude, latitude, tilt) {
-    setState(() {
-      _lon = longitude;
-      _lat = latitude;
-      _tilt = tilt;
-    });
-  }
+  // void onViewChanged(longitude, latitude, tilt) {
+  //   setState(() {
+  //     _lon = longitude;
+  //     _lat = latitude;
+  //     _tilt = tilt;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     final hotspotData =
         widget.destinationData['hotspotData'] as Map<String, dynamic>;
     final hotspotKeys = hotspotData.keys.toList();
-    // print(hotspotData);
-    Widget panorama = PanoramaViewer(
-      animSpeed: .1,
-      sensorControl: SensorControl.orientation,
-      onViewChanged: onViewChanged,
-      hotspots: hotspotKeys.asMap().entries.map((entry) {
-        // int index = entry.key;
-        String key = entry.value;
-        final hotspot = hotspotData[key];
-        // print(hotspot); showing index + 1  tho
-        return Hotspot(
-          latitude: hotspot['latitude'] ?? 0.0,
-          longitude: hotspot['longitude'] ?? 0.0,
-          width: 90,
-          height: 80,
-          widget: hotspotButton(
-            text: "Next scene",
-            icon: Icons.double_arrow,
-            onPressed: () => setState(() {
-              if (widget.destinationData['hotspotData'].isNotEmpty) {
-                _panoId = ((_panoId + 1) %
-                        widget.destinationData['hotspotData'].length)
-                    .toInt();
-              }
-            }),
-          ),
-        );
-      }).toList(),
-      child: Image.network(
-        hotspotData[hotspotKeys[_panoId]]['imagePath'],
-        fit: BoxFit.cover,
-      ),
-    );
 
     return Scaffold(
       body: Stack(
         children: [
-          panorama,
-          if (_showDebugInfo)
-            Positioned(
-              bottom: 10,
-              left: 10,
-              child: Text(
-                '${_lon.toStringAsFixed(3)}, ${_lat.toStringAsFixed(3)}, ${_tilt.toStringAsFixed(3)}',
-                style: const TextStyle(
-                  backgroundColor: Colors.black54,
-                  color: Colors.white,
+          PanoramaViewer(
+            animSpeed: .1,
+            sensorControl: SensorControl.orientation,
+            // onViewChanged: onViewChanged,
+            hotspots: hotspotKeys.asMap().entries.map((entry) {
+              String key = entry.value;
+              final hotspot = hotspotData[key];
+
+              return Hotspot(
+                latitude: hotspot['latitude'] ?? 0.0,
+                longitude: hotspot['longitude'] ?? 0.0,
+                width: 90,
+                height: 80,
+                widget: hotspotButton(
+                  text: "Next scene",
+                  icon: Icons.double_arrow,
+                  onPressed: () => setState(() {
+                    if (widget.destinationData['hotspotData'].isNotEmpty) {
+                      _panoId = ((_panoId + 1) %
+                              widget.destinationData['hotspotData'].length)
+                          .toInt();
+                    }
+                  }),
                 ),
-              ),
+              );
+            }).toList(),
+            child: Image.network(
+              hotspotData[hotspotKeys[_panoId]]['imagePath'],
+              fit: BoxFit.cover,
             ),
+          ),
+          // if (_showDebugInfo)
+          //   Positioned(
+          //     bottom: 10,
+          //     left: 10,
+          //     child: Text(
+          //       '${_lon.toStringAsFixed(3)}, ${_lat.toStringAsFixed(3)}, ${_tilt.toStringAsFixed(3)}',
+          //       style: const TextStyle(
+          //         backgroundColor: Colors.black54,
+          //         color: Colors.white,
+          //       ),
+          //     ),
+          //   ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _showDebugInfo = !_showDebugInfo;
-          });
-        },
-        child: Icon(_showDebugInfo ? Icons.visibility_off : Icons.visibility),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     setState(() {
+      //       _showDebugInfo = !_showDebugInfo;
+      //     });
+      //   },
+      //   child: Icon(_showDebugInfo ? Icons.visibility_off : Icons.visibility),
+      // ),
     );
   }
 
