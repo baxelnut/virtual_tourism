355ba9eb64ffe186d1f37273ecc9d68dfc45fc8f
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-10 21:59:03 +0700

    feat: add default and bazel profile image URLs to GlobalValues

--- lib/core/global_values.dart
+++ lib/core/global_values.dart
@@ -5,6 +5,12 @@ import 'package:provider/provider.dart';
 import 'theme/theme_provider.dart';
 
 class GlobalValues {
+  static const String defaultProfile =
+      'https://firebasestorage.googleapis.com/v0/b/virtual-tourism-7625f.appspot.com/o/users%2F.default%2Fprofile.png?alt=media&token=3471ca29-03b2-4bd7-a3fe-20dcc1810559';
+
+  static const String bazelPath =
+      'https://firebasestorage.googleapis.com/v0/b/virtual-tourism-7625f.appspot.com/o/users%2Fprofile%2FSEBcLELFH0NJpknkR1sygVD65rT2.jpg?alt=media&token=ecb1c829-52bb-4d3a-8675-b99c2c1f38f0';
+
   static const String placeholderPath =
       'https://hellenic.org/wp-content/plugins/elementor/assets/images/placeholder.png';
 
