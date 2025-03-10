2ce1aa73613655218fe914f6549c327ebbaff4e8
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-10 15:16:21 +0700

    feat: refactor FullViewList to use CustomScrollView and Sliver components for improved performance and UI

--- lib/src/components/util/full_view_list.dart
+++ lib/src/components/util/full_view_list.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-
 import '../cards/fit_width_card.dart';
 
 class FullViewList extends StatefulWidget {
   final String cardsTitle;
   final List<Map<String, dynamic>>? destinationData;
+
   const FullViewList({
     super.key,
     required this.cardsTitle,
@@ -19,39 +19,41 @@ class _FullViewListState extends State<FullViewList> {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
-    final Size screenSize = MediaQuery.of(context).size;
     final String photoUrlPlaceholder =
         'https://firebasestorage.googleapis.com/v0/b/virtual-tourism-7625f.appspot.com/o/users%2Fprofile%2FSEBcLELFH0NJpknkR1sygVD65rT2.jpg?alt=media&token=ecb1c829-52bb-4d3a-8675-b99c2c1f38f0';
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.cardsTitle,
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            if (widget.destinationData == null ||
-                widget.destinationData!.isEmpty)
-              SizedBox(
-                height: screenSize.height - 100,
-                child: Center(
-                  child: Text(
-                    'No data yet',
-                    style: theme.textTheme.bodyLarge,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(
+              widget.cardsTitle,
+            ),
+            floating: true,
+            snap: true,
+          ),
+          widget.destinationData == null || widget.destinationData!.isEmpty
+              ? SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Center(
+                    child: Text(
+                      'No data yet',
+                      style: theme.textTheme.bodyLarge,
+                    ),
+                  ),
+                )
+              : SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return FitWidthCard(
+                        userProfile: photoUrlPlaceholder,
+                        destinationData: widget.destinationData![index],
+                      );
+                    },
+                    childCount: widget.destinationData!.length,
                   ),
                 ),
-              )
-            else
-              ...widget.destinationData!.map(
-                (data) => FitWidthCard(
-                  userProfile: photoUrlPlaceholder,
-                  destinationData: data,
-                ),
-              ),
-          ],
-        ),
+        ],
       ),
     );
   }
