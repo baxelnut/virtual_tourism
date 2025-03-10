f5b203930866a1a3408e24b5f52b5e6f4deb5da4
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-10 15:10:13 +0700

    feat: refactor ContentTiles to improve structure and integrate MoreButton for enhanced admin functionality

--- lib/src/pages/admin/content_tiles.dart
+++ lib/src/pages/admin/content_tiles.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:virtual_tourism/src/components/destinations/destination_overview.dart';
 
+import '../../components/cards/more_button.dart';
 import '../../components/content/load_image.dart';
+import '../../components/destinations/destination_overview.dart';
 
 class ContentTiles extends StatelessWidget {
   final Map<String, dynamic> destinationData;
@@ -10,16 +11,10 @@ class ContentTiles extends StatelessWidget {
     required this.destinationData,
   });
 
-  String getImagePath(
-      Map<String, dynamic> destinationData, String placeholderPath) {
-    final thumbnailPath = destinationData["thumbnailPath"] ?? '';
-    final hotspotImagePath =
-        destinationData["hotspotData"]?["hotspot0"]?["imagePath"] ?? '';
-
-    if (thumbnailPath.isNotEmpty) return thumbnailPath;
-    if (hotspotImagePath.isNotEmpty) return hotspotImagePath;
-
-    return placeholderPath;
+  String getImagePath(String placeholderPath) {
+    return destinationData["thumbnailPath"] ??
+        destinationData["hotspotData"]?["hotspot0"]?["imagePath"] ??
+        placeholderPath;
   }
 
   @override
@@ -32,81 +27,21 @@ class ContentTiles extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4),
       child: GestureDetector(
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => DestinationOverview(
-                destinationData: destinationData,
-              ),
+        onTap: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DestinationOverview(
+              destinationData: destinationData,
             ),
-          );
-        },
+          ),
+        ),
         child: SizedBox(
           height: 110,
           width: screenSize.width,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(6),
-                child: LoadImage(
-                  imagePath: getImagePath(destinationData, placeholderPath),
-                  width: 100,
-                  height: 100,
-                ),
-              ),
+              _buildImage(getImagePath(placeholderPath)),
               const SizedBox(width: 14),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 200,
-                            child: Text(
-                              destinationData['destinationName'],
-                              style: theme.textTheme.headlineSmall,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Icon(
-                              Icons.more_vert_rounded,
-                              size: 20,
-                              color: theme.colorScheme.onSurface,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    _buildStats(
-                      avgRating: 3.3,
-                      totalReviews: 4,
-                      totalView: 10,
-                      theme: theme,
-                      screenSize: screenSize,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 24, bottom: 8),
-                      child: Text(
-                        "${destinationData['category']} • ${destinationData['subcategory']}",
-                        style: theme.textTheme.labelSmall,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              _buildTextContent(theme, screenSize),
             ],
           ),
         ),
@@ -114,44 +49,97 @@ class ContentTiles extends StatelessWidget {
     );
   }
 
-  Widget _buildStats({
-    required double avgRating,
-    required int totalReviews,
-    required int totalView,
-    required ThemeData theme,
-    required Size screenSize,
-  }) {
+  Widget _buildImage(String imagePath) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(6),
+      child: LoadImage(
+        imagePath: imagePath,
+        width: 100,
+        height: 100,
+      ),
+    );
+  }
+
+  Widget _buildTextContent(ThemeData theme, Size screenSize) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildHeader(theme),
+          _buildStats(theme, screenSize),
+          _buildText(
+            "${destinationData['category']} • ${destinationData['subcategory']}",
+            theme.textTheme.labelSmall,
+            padding: const EdgeInsets.only(right: 24, bottom: 8),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(ThemeData theme) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            child: Text(
+              destinationData['destinationName'],
+              style: theme.textTheme.headlineSmall,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          MoreButton(
+            isAdmin: true,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStats(ThemeData theme, Size screenSize) {
+    final stats = [
+      {'icon': Icons.star, 'value': '3.3'},
+      {'icon': Icons.comment, 'value': '4'},
+      {'icon': Icons.remove_red_eye_rounded, 'value': '10'},
+    ];
+
     return SizedBox(
       width: screenSize.width / 3,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.star,
-            size: 16,
-            color: theme.colorScheme.onSurface,
-          ),
-          Text(
-            avgRating.toString(),
-          ),
-          Icon(
-            Icons.comment,
-            size: 16,
-            color: theme.colorScheme.onSurface,
-          ),
-          Text(
-            totalReviews.toString(),
-          ),
-          Icon(
-            Icons.remove_red_eye_rounded,
-            size: 16,
-            color: theme.colorScheme.onSurface,
-          ),
-          Text(
-            totalView.toString(),
-          ),
-        ],
+        children: stats.map(
+          (stat) {
+            return Row(
+              children: [
+                Icon(
+                  stat['icon'] as IconData,
+                  size: 16,
+                  color: theme.colorScheme.onSurface,
+                ),
+                const SizedBox(width: 4),
+                Text(
+                  stat['value'].toString(),
+                ),
+              ],
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
+
+  Widget _buildText(String text, TextStyle? style, {EdgeInsets? padding}) {
+    return Padding(
+      padding: padding ?? EdgeInsets.zero,
+      child: Text(
+        text,
+        style: style,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
       ),
     );
   }
