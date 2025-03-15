5dc32c07aaf6a126ba65745a0b8bffa8931d6c13
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-16 04:14:03 +0700

    feat: implement pull-to-refresh functionality in FullViewList for enhanced user experience

--- lib/presentation/widgets/utils/full_view_list.dart
+++ lib/presentation/widgets/utils/full_view_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import '../../../core/global_values.dart';
+import '../../../core/nuke_refresh.dart';
 import '../cards/fit_width_card.dart';
 
 class FullViewList extends StatefulWidget {
@@ -23,37 +25,46 @@ class _FullViewListState extends State<FullViewList> {
     final String bazelPath = GlobalValues.bazelPath;
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: Text(
-              widget.cardsTitle,
+      body: LiquidPullToRefresh(
+        onRefresh: () async {
+          await NukeRefresh.forceRefresh(context, 1);
+        },
+        height: 120,
+        color: theme.colorScheme.primary,
+        backgroundColor: theme.colorScheme.surface,
+        animSpeedFactor: 4,
+        borderWidth: 3,
+        showChildOpacityTransition: false,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: Text(widget.cardsTitle),
+              floating: true,
+              snap: true,
             ),
-            floating: true,
-            snap: true,
-          ),
-          widget.destinationData == null || widget.destinationData!.isEmpty
-              ? SliverFillRemaining(
-                  hasScrollBody: false,
-                  child: Center(
-                    child: Text(
-                      'No data yet',
-                      style: theme.textTheme.bodyLarge,
+            widget.destinationData == null || widget.destinationData!.isEmpty
+                ? SliverFillRemaining(
+                    hasScrollBody: false,
+                    child: Center(
+                      child: Text(
+                        'No data yet',
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                    ),
+                  )
+                : SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        return FitWidthCard(
+                          userProfile: bazelPath,
+                          destinationData: widget.destinationData![index],
+                        );
+                      },
+                      childCount: widget.destinationData!.length,
                     ),
                   ),
-                )
-              : SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      return FitWidthCard(
-                        userProfile: bazelPath,
-                        destinationData: widget.destinationData![index],
-                      );
-                    },
-                    childCount: widget.destinationData!.length,
-                  ),
-                ),
-        ],
+          ],
+        ),
       ),
     );
   }
