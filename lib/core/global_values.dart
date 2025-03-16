95277c97a68b61abe7f0a7697901795f3c33bf60
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-16 09:25:24 +0700

    refactor: update bazelPath to use a new default profile image URL

--- lib/core/global_values.dart
+++ lib/core/global_values.dart
@@ -8,8 +8,9 @@ class GlobalValues {
   static const String defaultProfile =
       'https://firebasestorage.googleapis.com/v0/b/virtual-tourism-7625f.appspot.com/o/users%2F.default%2Fprofile.png?alt=media&token=3471ca29-03b2-4bd7-a3fe-20dcc1810559';
 
+  // temp
   static const String bazelPath =
-      'https://firebasestorage.googleapis.com/v0/b/virtual-tourism-7625f.appspot.com/o/users%2Fprofile%2FSEBcLELFH0NJpknkR1sygVD65rT2.jpg?alt=media&token=ecb1c829-52bb-4d3a-8675-b99c2c1f38f0';
+      'https://firebasestorage.googleapis.com/v0/b/virtual-tourism-7625f.appspot.com/o/users%2F.default%2Fprofile3.jpg?alt=media&token=3d6e8de5-5d16-440d-ae5f-fc7013be3ebb';
 
   static const String placeholderPath =
       'https://hellenic.org/wp-content/plugins/elementor/assets/images/placeholder.png';
