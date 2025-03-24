228ec838e3e8ba4680ac405323e73f162834573c
Basilius Tengang <basiliustengang24@gmail.com>
2025-03-24 22:33:50 +0700

    fix: rename panoId to panoIndex for clarity and update hotspot handling in TourScreen

--- lib/presentation/widgets/content/tour_screen.dart
+++ lib/presentation/widgets/content/tour_screen.dart
@@ -13,7 +13,7 @@ class TourScreen extends StatefulWidget {
 }
 
 class _TourScreenState extends State<TourScreen> {
-  int _panoId = 0;
+  int _panoIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +21,16 @@ class _TourScreenState extends State<TourScreen> {
         widget.destinationData['hotspotData'] as Map<String, dynamic>;
     final hotspotKeys = hotspotData.keys.toList();
 
-    double clampedLat = (hotspotData[hotspotKeys[_panoId]]['latitude'] ?? 0.0)
-        .clamp(-90.0, 90.0);
+    double clampedLat =
+        (hotspotData[hotspotKeys[_panoIndex]]['latitude'] ?? 0.0)
+            .clamp(-90.0, 90.0);
     double clampedLon =
-        (hotspotData[hotspotKeys[_panoId]]['longitude'] ?? 0.0) % 360;
+        (hotspotData[hotspotKeys[_panoIndex]]['longitude'] ?? 0.0) % 360;
+
+    String? hotDesc =
+        widget.destinationData['hotspotData']['hotspot$_panoIndex']['hotDesc'];
+    double estimatedHeight = ((hotDesc?.length ?? 0) / 50).ceil() * 20.0;
+    double maxHeight = 1000;
 
     return Scaffold(
       body: Stack(
@@ -44,16 +50,24 @@ class _TourScreenState extends State<TourScreen> {
                     icon: Icons.double_arrow,
                     onPressed: () => setState(() {
                       if (widget.destinationData['hotspotData'].isNotEmpty) {
-                        _panoId = ((_panoId + 1) %
+                        _panoIndex = ((_panoIndex + 1) %
                                 widget.destinationData['hotspotData'].length)
                             .toInt();
                       }
                     }),
                   ),
                 ),
+              if (hotDesc != null)
+                Hotspot(
+                  latitude: clampedLat - 10,
+                  longitude: clampedLon,
+                  width: 400,
+                  height: estimatedHeight.clamp(50, maxHeight),
+                  widget: hotspotDescription(hotDesc: hotDesc),
+                ),
             ],
             child: Image.network(
-              hotspotData[hotspotKeys[_panoId]]['imagePath'],
+              hotspotData[hotspotKeys[_panoIndex]]['imagePath'],
               fit: BoxFit.cover,
             ),
           ),
@@ -87,10 +101,34 @@ class _TourScreenState extends State<TourScreen> {
               borderRadius: BorderRadius.all(Radius.circular(4)),
             ),
             child: Center(
-              child: Text(text, textAlign: TextAlign.center),
+              child: Text(
+                text,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ),
       ],
+    );
+  }
+
+  Widget hotspotDescription({
+    required String hotDesc,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.black38,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: SingleChildScrollView(
+        child: Text(
+          hotDesc,
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 10,
+        ),
+      ),
     );
   }
 }
